2e1b107fddb6c82c1e91057759fd9ff3d0c46294
Nutchanon Ninyawee <me@nutchanon.org>
2020-07-13 10:52:29 +0700

    add function and variable assignment

--- basic.py
+++ basic.py
@@ -1,2 +1,13 @@
 # https://help.semmle.com/QL/learn-ql/python/ql-for-python.html
+error = True
 if error: pass
+
+x = None
+
+def reset_this_world():
+    return 123
+
+for var in range(123):
+    pass
+else:
+    x = 12
